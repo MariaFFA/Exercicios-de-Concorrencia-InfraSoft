54adac825bae79a036222ec7d5e674c9b930b401
Tulio Oliveira <127243520+tuliooarauj@users.noreply.github.com>
2024-06-19 14:35:26 -0300

    Update Main_sync.java

--- src/problema_2/Main_sync.java
+++ src/problema_2/Main_sync.java
@@ -11,6 +11,9 @@ public class Main_sync {
 
         public void run() {
             System.out.println(nomeCarro+ " tentando atravessar a ponte...");
+            System.out.println("...");
+            System.out.println(nomeCarro+ " atravessou a ponte.");
+            System.out.println("======================");
             try {
                 Thread.sleep(1);
             }
@@ -31,50 +34,27 @@ public class Main_sync {
 
         Carro1.start();
         Carro1.join();
-        System.out.println("Carro 1 Atravessou");
-
-        System.out.println("======================");
 
         Carro2.start();
         Carro2.join();
-        System.out.println("Carro 2 Atravessou");
-
-        System.out.println("======================");
 
         Carro3.start();
         Carro3.join();
-        System.out.println("Carro 3 Atravessou");
-
-        System.out.println("======================");
 
         Carro4.start();
         Carro4.join();
-        System.out.println("Carro 4 Atravessou");
-
-        System.out.println("======================");
 
         Carro5.start();
         Carro5.join();
-        System.out.println("Carro 5 Atravessou");
-
-        System.out.println("======================");
 
         Carro6.start();
         Carro6.join();
-        System.out.println("Carro 6 Atravessou");
-
-        System.out.println("======================");
 
         Carro7.start();
         Carro7.join();
-        System.out.println("Carro 7 Atravessou");
-
-        System.out.println("======================");
 
         Carro8.start();
         Carro8.join();
-        System.out.println("Carro 8 Atravessou");
-
 
     }
 }
