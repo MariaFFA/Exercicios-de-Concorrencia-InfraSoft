9a354e67dcade9086006866585f5e646c34ec2d4
Tulio Oliveira de Araujo <toa@WINDOWS.CIN.UFPE.BR>
2024-06-19 11:48:53 -0300

    Início Problema 2

---
+++ src/problema_2/Main.java
@@ -0,0 +1,55 @@
+package problema_2;
+
+public class Main {
+
+    public static class FioThread extends Thread {
+    //        public int permit;
+        public String nomeCarro;
+
+        public FioThread(String nomeCarro) {
+            this.nomeCarro = nomeCarro;
+        }
+
+        public void run() {
+            System.out.println(nomeCarro+ " tentando atravessar a ponte...");
+            try {
+                Thread.sleep(1);
+            }
+            catch (InterruptedException ignored){}
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        FioThread Carro1 = new FioThread("Carro 1");
+        FioThread Carro2 = new FioThread("Carro 2");
+        FioThread Carro3 = new FioThread("Carro 3");
+        FioThread Carro4 = new FioThread("Carro 4");
+        FioThread Carro5 = new FioThread("Carro 5");
+        FioThread Carro6 = new FioThread("Carro 6");
+        FioThread Carro7 = new FioThread("Carro 7");
+        FioThread Carro8 = new FioThread("Carro 8");
+
+        Carro1.start();
+        Carro2.start();
+        Carro3.start();
+        Carro4.start();
+        Carro5.start();
+        Carro6.start();
+        Carro7.start();
+        Carro8.start();
+
+        Carro1.join();
+        System.out.println("Carro 1 Atravessou");
+        Carro2.join();
+        System.out.println("Carro 2 Atravessou");
+        Carro3.join();
+        Carro4.join();
+        Carro5.join();
+        Carro6.join();
+        Carro7.join();
+        Carro8.join();
+
+    }
+}
+
